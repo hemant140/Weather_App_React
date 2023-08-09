78f9f4e54eccc6c82f325e10dd9f7aef784a3395
Hemant <hemo.hm576@gmail.com>
2023-08-09 16:38:33 +0530

    done

--- README.md
+++ README.md
@@ -1,70 +1,38 @@
-# Getting Started with Create React App
+# Weather Forecast App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Welcome to the Weather Forecast App! This React project allows you to check the weather forecast for a specific city.
 
-## Available Scripts
+## Prerequisites
 
-In the project directory, you can run:
+Before you get started, make sure you have the following installed:
 
-### `npm start`
+- Node.js (v18.12.1)
+- React (v18.2.0)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Getting Started
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Follow these steps to get the project up and running:
 
-### `npm test`
+1. Clone the repository to your local machine using:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   ```bash
+   git clone https://github.com/hemant140/Weather_app_React
 
-### `npm run build`
+2. cd weather-forecast-app
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+3. npm install
+ 
+4. npm start
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+This will launch the app in your default web browser, and you can now start using the Weather Forecast App to check weather details for different cities.
 
-### `npm run eject`
+Contributing
+If you'd like to contribute to this project, feel free to fork the repository and submit pull requests. Contributions are welcome!
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Contact
+For any questions or issues, please feel free to reach out to me:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+##Name: Hemant Singh
+##Portfolio: https://hemant14.netlify.app/
+##Thank you for checking out the Weather Forecast App!
--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@
 //  Visit My PortFolio - https://hemant14.netlify.app/
 
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import cloudSunImage from './images/cloudy_1163661.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faTint
-} from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import cloudSunImage from './images/sun_9361662.png'
+import sunnyImage from './images/sun_4814268.png'
+import cloudyImage from './images/clouds_414927.png'
+import rainyImage from './images/rain_2469994.png'
+import snowyImage from './images/snowy_1779887.png'
+import FeelLike from './images/hot.png'
+import humidity from './images/humidity.png'
+
 const API_KEY = '602a3c09bad82e8f4265c87eb7b54734';
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
   const [error, setError] = useState(null);
   const [hidden, setHidden] = useState(false);
   const [loading, setLoading] = useState(false);
+  const weatherCondition = weatherData && weatherData.weather[0].main;
+  let weatherImageUrl;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setHidden(true)
@@ -54,35 +60,69 @@ function App() {
     setLocation("");
     setWeatherData(null);
     setError('');
-
   }
+
+  if (weatherCondition === 'Clear') {
+    weatherImageUrl = sunnyImage;
+  } else if (weatherCondition === 'Clouds') {
+    weatherImageUrl = cloudyImage;
+  } else if (weatherCondition === 'Rain') {
+    weatherImageUrl = rainyImage;
+  } else if (weatherCondition === 'Snow') {
+    weatherImageUrl = snowyImage;
+  } else {
+    weatherImageUrl = cloudSunImage;
+  }
+
+  // console.log(weatherData)
 
   return (
     <div className="App">
       <div className="inputFormDiv">
-        {loading && <div class="loader">Loading...</div>}
-
         <div className='heading'>
-          {hidden && <p className='backIcon' onClick={backIcon}><i class="fas fa-arrow-left"></i></p>}
+          {hidden && <p className='backIcon' onClick={backIcon}><i class="fas fa-arrow-left "></i></p>}
           {hidden ? <p className='headingName'>Weather App</p> : <p className='headingName'>Weather App</p>}
         </div>
-        {error && <p className="error">{error}</p>}
+        {error &&
+          <div className="error">
+            <p >{error}</p>
+            <button onClick={backIcon} className='backButton'><i class="fas fa-arrow-left arrowIcon"></i> Back</button>
+          </div>
+        }
+        {loading && <div class="loaderDiv"></div>}
         {hidden ?
           (weatherData && (
             <div className='weatherCrad'>
-              <div className='weatherImg'><img src={cloudSunImage} alt="" /></div>
+              <div className='weatherImg'><img src={weatherImageUrl} alt="" /></div>
               <span className='temp'>{weatherData.main.temp.toFixed(0)} <span>°C</span></span>
               <p className='description'>{weatherData.weather[0].description}</p>
-              <span className='location'><i class="fas fa-map-marker-alt" style={{ marginRight: "3px" }}></i>{weatherData.name}, {weatherData.sys.country}</span>
+              <span className='location'><i class="fas fa-map-marker-alt" style={{ marginRight: "5px" }}></i>{weatherData.name}, {weatherData.sys.country}</span>
               <div className='footerCard'>
-                <p><i class="fas fa-temperature-low"></i> {weatherData.main.feels_like.toFixed(0)}% °C <br />Feel Like</p>
-                <p><FontAwesomeIcon icon={faTint} style={{ marginRight: "3px" }} />{weatherData.main.humidity}% <br /> Humidity</p>
+                <div class="footer-text-1">
+                  <div class="icon-f-1"><img src={FeelLike} alt="" width="30" height="30" /></div>
+                  <div class="icon-f-2">
+                    <span class="c-17">{weatherData.main.feels_like.toFixed(0)} °C</span>
+                    <span class="feel-like">Feel like</span>
+                  </div>
+                </div>
+                <div class="footer-text-2">
+                  <div class="icon-f-1"><img src={humidity} alt="" width="30" height="30" /></div>
+                  <div class="icon-f-2">
+                    <span class="c-17">{weatherData.main.humidity}%</span>
+                    <span class="feel-like">Humidity</span>
+                  </div>
+                </div>
               </div>
             </div>
           )
           ) : (
-            < form className="weatherForm" onSubmit={handleSubmit}>
+            <form className="weatherForm" onSubmit={handleSubmit}>
               <input required type="text" value={location} placeholder="Enter location" onChange={(e) => setLocation(e.target.value)} />
+              <div class="form-control">
+                <div class="line-1"></div>
+                <div class="line-text"><p>Or</p></div>
+                <div class="line-2"></div>
+              </div>
               <button type="submit">Get Device Location</button>
             </form>
           )
